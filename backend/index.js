514706de15471047e0841fd5f69c1f78cab92628
Puligattu Sreevatsav <sreevatsavpuligattu@gmail.com>
2025-05-18 16:41:40 +0530

    fix(backend): read from .env

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,4 @@
-const dotenv = require("dotenv");
-const envFile = `.env.${process.env.NODE_ENV || "development"}`;
-dotenv.config({ path: envFile });
+require("dotenv").config();
 
 const eventsRoutes = require("./routes/events.route");
 
